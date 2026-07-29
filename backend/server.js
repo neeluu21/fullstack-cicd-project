07043bc9d67689e0fb67020ad5e530ee9a20381c
Ubuntu <ubuntu@ip-172-31-15-175.ap-south-1.compute.internal>
2026-07-29 18:15:08 +0000

    Initial Full Stack Project

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+mongoose
+  .connect(process.env.MONGO_URL || "mongodb://localhost:27017/studentdb")
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+
+app.get("/api", (req, res) => {
+  res.json({
+    message: "Backend Running Successfully",
+  });
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
